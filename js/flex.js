3804c1c3788e2e499c37f4a033b3426c366fb112
Marco Tereso <marco.tereso@gmail.com>
2022-10-13 22:17:06 +0100

    wrap e justify

--- js/flex.js
+++ js/flex.js
@@ -1,3 +1,9 @@
 function fd(valor){
 	document.getElementById('container').style.flexDirection=valor;
 }
+function fw(valor){
+	document.getElementById('container').style.flexWrap=valor;
+}
+function fj(valor){
+	document.getElementById('container').style.justifyContent=valor;
+}
